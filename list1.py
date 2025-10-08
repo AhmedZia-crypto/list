9555919fea209cd2b3f37ab587fabd5db0605eb3
AhmedZia-crypto <zia14fox@gmail.com>
2025-10-08 16:29:16 +0300

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,11 @@
+list1 = [12,13,16,89,100,35,77]
+print(list1)
+list1.append(400)
+print(list1)
+list2 = ["amit", "ahmed", 45,89,True,False]
+print(list2)
+l = len(list1)
+print(l)
+print(list[4])
+print(list1*3)
+print(list1[::-1])
